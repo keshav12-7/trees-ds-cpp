05a349574de666aa59a5dbd48e5c3c0c335ab07d
keshav verma <keshavcode1@gmail.com>
2025-08-15 16:12:51 +0530

    some changes

---
+++ l103/l103.dfs-binarytreemaking.cpp
@@ -0,0 +1,33 @@
+# include <bits/stdc++.h>
+using namespace std;
+class node{
+    public:
+    int data;
+    node*left,*right;
+    node(int val){
+        data = val;
+        left = right = NULL;
+    }
+};
+node *binarytree(){
+    int x ;
+    cin>>x;
+    if(x == -1){
+        return NULL;
+
+    }
+    node *temp = new node(x);
+    cout<<"enter left child of "<<x<<" : ";
+    temp->left = binarytree(); // left side 
+    cout<<"enter right child of "<<x<<" : ";
+    temp->right = binarytree(); // right side
+    return temp;
+
+
+}
+int main(){
+    cout<<"enter the root node : ";
+    node*root;
+    root = binarytree();
+    return 0;
+}
